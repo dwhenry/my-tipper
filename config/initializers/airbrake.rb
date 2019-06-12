653af5970c41231cb52c683723fa90382dc4730b
David Henry <david@decoybecoy.com>
2019-06-13 00:39:54 +0100

    Update gem version and initializer

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,7 +1,8 @@
 require 'rake'
-require 'airbrake/rake_handler'
+require 'airbrake/rake'
 
 Airbrake.configure do |config|
-  config.api_key = '88bbc6aa8baed00192c1e0a4f6773061'
-  config.rescue_rake_exceptions = true
+  config.project_id = '115806'
+  config.project_key = '88bbc6aa8baed00192c1e0a4f6773061'
+  # config.rescue_rake_exceptions = true
 end
