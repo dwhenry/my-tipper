6e418de2b5848ebc590037b9d32a260901d4b98c
David Henry <david@decoybecoy.com>
2019-06-16 21:26:13 +0100

    Better forms for login and league create/join

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,9 +2,10 @@ class HomeController < ApplicationController
   # before_action :authenticate_user!
 
   def index
-    if event == '2015' && current_user
+    if event == '2019' && current_user
       # ensure the user is in the current event as soon as they log in (hack)
-      Player.find_or_create_by(user: current_user, league_id: League.find_by(name: '2015').id)
+      league = League.find_or_create_by(name: '2019', event: true, public: true).id
+      Player.find_or_create_by(user: current_user, league_id: league)
     end
   end
 end
