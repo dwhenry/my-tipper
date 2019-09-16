262f42cb141c1d0fb2d06dd572fb460e559b5b99
David Henry <david@decoybecoy.com>
2019-09-16 14:38:41 +0100

    Move back to send mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,12 +85,13 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'my-tipper.herokuapp.com' }
 
   config.action_mailer.smtp_settings = {
-    :port           => ENV['MAILGUN_SMTP_PORT'],
-    :address        => ENV['MAILGUN_SMTP_SERVER'],
-    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :port           => 587,
+    :address        => 'smtp.sendgrid.net',
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
     :domain         => 'my-tipper.herokuapp.com',
     :authentication => :plain,
+    :enable_starttls_auto => true
   }
 
   # config.action_mailer.smtp_settings = {
