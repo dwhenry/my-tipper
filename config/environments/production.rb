5d8869d6ed5ee534b0c54991cc30ee12b8028223
David Henry <david@decoybecoy.com>
2019-09-16 14:17:18 +0100

    Move mail to mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,12 +85,21 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'my-tipper.herokuapp.com' }
 
   config.action_mailer.smtp_settings = {
-    :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['SENDGRID_API_KEY'],
-    :domain => 'decoybecoy.com',
-    :address => 'smtp.sendgrid.net',
-    :port => 587,
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'my-tipper.herokuapp.com',
     :authentication => :plain,
-    :enable_starttls_auto => true
   }
+
+  # config.action_mailer.smtp_settings = {
+  #   :user_name => ENV['SENDGRID_USERNAME'],
+  #   :password => ENV['SENDGRID_API_KEY'],
+  #   :domain => 'decoybecoy.com',
+  #   :address => 'smtp.sendgrid.net',
+  #   :port => 587,
+  #   :authentication => :plain,
+  #   :enable_starttls_auto => true
+  # }
 end
