555e3c4a07d509df429588f6499f08a8b0f58f54
David Henry <david@decoybecoy.com>
2019-09-16 14:08:53 +0100

    More mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,10 +77,14 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'my-tipper.herokuapp.com' }
 
   config.action_mailer.delivery_method = :smtp
-  ActionMailer::Base.smtp_settings = {
+  config.action_mailer.perform_deliveries = true
+  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.default_url_options = { host: 'my-tipper.herokuapp.com' }
+
+  config.action_mailer.smtp_settings = {
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_API_KEY'],
     :domain => 'decoybecoy.com',
